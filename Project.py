62f0a4bb51bc3cd7415d36dce55c8e1a3b07ee23
조성혁 <tjdgur011@naver.com>
2020-11-08 21:37:11 +0900

    (재반영)1주차-실거래가내역 파일 읽기 기능 추가

--- Project.py
+++ Project.py
@@ -1,1 +1,23 @@
 #프로그래밍언어 프로그램 개발
+import openpyxl
+
+filename = "abc.xlsx"
+filedata = openpyxl.load_workbook(filename)
+detaildata = filedata.worksheets[0]
+
+data = []
+for row in detaildata.rows:
+    data.append([
+        row[0].value,
+        row[1].value,
+        row[2].value,
+        row[3].value,
+        row[4].value,
+        row[5].value,
+        row[6].value,
+        row[7].value,
+        row[8].value,
+        row[9].value
+    ])
+
+print(data)
